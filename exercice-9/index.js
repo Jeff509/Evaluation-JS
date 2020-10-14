815a5573ca6e6ec8f6031171d0fb8069a0a306e5
REGISTRE JEFF <=>
2020-10-14 23:02:26 +0200

    feat(check palindrom)palindrom

--- exercice-9/index.js
+++ exercice-9/index.js
@@ -1,1 +1,39 @@
 'use strict';
+
+/**
+ * function that checks the parameter
+ * @constructor
+ * @param {string} str - A character string.
+ */
+
+var checkPalindrome = function checkPalindrome(str) {
+    this.str = str;
+}
+
+/**
+ * Check if palindrome.
+ */
+
+checkPalindrome.prototype.isPalindrome = function() {
+        var str = this.str;
+        var clean = (str) => str.toLowerCase().replace(/[\W_]/g, '');
+        var cleanStr = clean(str);
+
+        for (var i = 0; i < cleanStr.length / 2; i++) {
+            if (cleanStr[i] !== cleanStr[cleanStr.length - 1 - i]) {
+                alert(cleanStr + ' is not a palindrome');
+                return false;
+            }
+        }
+        alert(cleanStr + ' is a palindrome');
+        return true;
+    }
+    /**
+     * Run
+     */
+
+checkPalindrome.prototype.run = function() {
+    this.isPalindrome();
+}
+var palindrome1 = new checkPalindrome('rassrah');
+palindrome1.run();
