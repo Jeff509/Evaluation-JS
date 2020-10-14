83b3e411c2233b06ea8f34cc09b569b92a9c1f95
REGISTRE JEFF <=>
2020-10-14 23:27:22 +0200

    feat(checkphone)add checkphonenumber

--- exercice-6/index.js
+++ exercice-6/index.js
@@ -1,1 +1,19 @@
-'use strict';
+'use strict'
+
+/**
+ * Check Phone Number
+ * @param {String} phoneNumber
+ * @return {boolean} isValid
+ */
+var checkPhoneNumber = function checkPhoneNumber(phoneNumber) {
+    if (!phoneNumber.search(/^((\+)33|0)[1|6|7](\d{2}){4}$/g)) {
+        return true;
+    }
+
+    return false;
+}
+
+console.log(checkPhoneNumber('0641831714'));
+console.log(checkPhoneNumber('0141831714'));
+console.log(checkPhoneNumber('0641831716'));
+console.log(checkPhoneNumber('+33641781716'));
