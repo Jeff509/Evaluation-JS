8285b8e8aed68729d2ceddc342b4b3fe60c4839b
REGISTRE JEFF <=>
2020-10-14 20:22:52 +0200

    feat(drawbar)add drawbar

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,1 +1,37 @@
 'use strict';
+
+/**
+ * @constructor
+ * @param {number} sum - full-bar length.
+ * @param {number} nbr - loaded part bar.
+ */
+var drawBar = function drawBar(sum, nbr) {
+        this.sum = sum;
+        this.nbr = nbr;
+    }
+    /**
+     * Create And display Bar
+     */
+
+drawBar.prototype.display = function() {
+    var progress = document.createElement('div');
+    var myBar = document.createElement('div');
+    progress.style.width = this.sum + '%';
+    progress.style.backgroundColor = 'lightblue';
+    progress.style.height = '30px';
+    myBar.style.width = this.nbr + '%';
+    myBar.style.backgroundColor = 'orange';
+    myBar.style.height = '30px';
+    progress.appendChild(myBar);
+    document.body.appendChild(progress);
+}
+
+/**
+ * Run
+ */
+
+drawBar.prototype.run = function() {
+    this.display();
+}
+var loadingBar = new drawBar(100, 10);
+loadingBar.run();
